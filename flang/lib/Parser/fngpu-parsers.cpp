876932c5bfc997e84766fcc1de548ce8293e6883
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2026-07-18 12:09:54 +0100

    Adding in fngpu parser

--- flang/lib/Parser/fngpu-parsers.cpp
+++ flang/lib/Parser/fngpu-parsers.cpp
@@ -6,34 +6,34 @@ namespace Fortran::parser {
 
 // 1. Parse the enums: "HOST" or "DEVICE"
 constexpr auto fngpuPackTarget =
-    "HOST" >> pure(FngpuPackTarget::Host) ||
-    "DEVICE" >> pure(FngpuPackTarget::Device);
+    "HOST" >> pure(FnGPUPackTarget::Host) ||
+    "DEVICE" >> pure(FnGPUPackTarget::Device);
 
 // 2. Parse a single pack item: Name : Target
-TYPE_PARSER(construct<FngpuPackClause::Item>(
+TYPE_PARSER(construct<FnGPUPackClause::Item>(
     name / ":", fngpuPackTarget))
 
 // 3. Parse the whole PACK clause: PACK ( item1, item2, ... )
-TYPE_PARSER(construct<FngpuPackClause>(
-    "PACK" >> parenthesized(nonemptyList(Parser<FngpuPackClause::Item>{}))))
+TYPE_PARSER(construct<FnGPUPackClause>(
+    "PACK" >> parenthesized(nonemptyList(Parser<FnGPUPackClause::Item>{}))))
 
 // 4. Parse the TILE clause: TILE ( int, int, ... )
-TYPE_PARSER(construct<FngpuTileClause>(
+TYPE_PARSER(construct<FnGPUTileClause>(
     "TILE" >> parenthesized(nonemptyList(scalarIntConstantExpr))))
 
 // 5. Parse any clause variant
-TYPE_PARSER(construct<FngpuClause>(Parser<FngpuTileClause>{}) ||
-            construct<FngpuClause>(Parser<FngpuPackClause>{}))
+TYPE_PARSER(construct<FnGPUClause>(Parser<FnGPUTileClause>{}) ||
+            construct<FnGPUClause>(Parser<FnGPUPackClause>{}))
 
 // 6. Parse the directive body: PARALLEL [clause...]
 // Note: The prescanner handles the "!$FNGPU" sentinel, so this parser
 // only sees the tokens that come after it.
-TYPE_PARSER(construct<FngpuParallelDirective>(
-    "PARALLEL" >> many(Parser<FngpuClause>{})))
+TYPE_PARSER(construct<FnGPUParallelDirective>(
+    "PARALLEL" >> many(Parser<FnGPUClause>{})))
 
 // 7. Tie it together: The Directive + the Fortran Loop
-TYPE_PARSER(construct<FngpuConstruct>(
-    statement(Parser<FngpuParallelDirective>{}),
+TYPE_PARSER(construct<FnGPUConstruct>(
+    statement(Parser<FnGPUParallelDirective>{}),
     indirect(Parser<ExecutableConstruct>{})))
 
 } // namespace Fortran::parser
